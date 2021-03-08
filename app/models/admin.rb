ed260a01a2a37150286ce8dced3523eb63d97e6e
Fernandocgomez <fernandocgomez@live.com>
2021-03-07 22:59:50 -0600

    Add 8 products - admin - 3 collections

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,7 @@
 class Admin < ApplicationRecord
     has_many :products, dependent: :delete_all
     has_many :orders, dependent: :delete_all
+    has_many :collections, dependent: :delete_all
 
     has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_03_07_003427) do
 
   create_table "collections", force: :cascade do |t|
     t.string "name", null: false
+    t.integer "admin_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -97,9 +98,10 @@ ActiveRecord::Schema.define(version: 2021_03_07_003427) do
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
+    t.string "image"
     t.float "price", null: false
     t.float "sale_price"
-    t.string "size", default: [], null: false, array: true
+    t.float "size", default: [], null: false, array: true
     t.integer "admin_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

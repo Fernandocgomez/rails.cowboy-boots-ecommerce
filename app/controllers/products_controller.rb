284ca205e62a440c61ebf1470081f5241cc9331a
Fernandocgomez <fernandocgomez@live.com>
2021-03-08 23:26:00 -0600

    Product: validations, routes, controller methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,11 +5,18 @@ class ProductsController < ApplicationController
         if(product)
             render json: { "res" => product }, status: 200
         else
-            render json: { "res" => "Not found" }, status: 404
+            render json: { "res" => "not found" }, status: 404
         end
     end
 
     def get_products_by_collection
-        
+        collection = Collection.find_by_id(params[:collection_id])
+        if(collection)
+            products = collection.products
+            render json: { "res" => products }, status: 200
+        else
+            render json: { "res" => "not found" }, status: 404
+        end
     end
+
 end
